bc7d935ec157c6a386a337077dd33d88ae9ff5dd
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2019-09-21 15:49:15 -0500

    fix routing error and remove guard from rrcc page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,22 @@
-
-
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AuthGuard } from './@auth/auth.guard';
 
 const routes: Routes = [
+  //    remove app before path
+  //  app/rrcp/rrcp.module -> ./rrcp/rrcp.module
   {
-    canActivate: [AuthGuard],
     path: 'rrcp',
-    loadChildren: () => import('app/rrcp/rrcp.module').then(m => m.RRCpModule),
+    loadChildren: () => import('./rrcp/rrcp.module').then(m => m.RRCpModule),
   },
   {
     canActivate: [AuthGuard],
     path: 'pages',
-    loadChildren: () => import('app/pages/pages.module').then(m => m.PagesModule),
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
   },
   {
     path: 'auth',
-    loadChildren: () => import('app/@auth/auth.module').then(m => m.AuthModule),
+    loadChildren: () => import('./@auth/auth.module').then(m => m.AuthModule),
   },
   { path: '', redirectTo: 'rrcp', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages' },
