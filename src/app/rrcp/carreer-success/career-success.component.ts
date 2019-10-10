c851a8f3597314de0976f371147d884fbda71799
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2019-10-10 00:51:16 -0500

    menu auto close fix

--- src/app/rrcp/carreer-success/career-success.component.ts
+++ src/app/rrcp/carreer-success/career-success.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { takeWhile } from 'rxjs/operators';
 import { Device, DevicesData } from '../../@core/interfaces/iot/devices';
 import { SolarEnergyStatistics, SolarData } from '../../@core/interfaces/iot/solar';
-
+import { NbSidebarService } from '@nebular/theme';
+import { LayoutService } from '../../@core/utils';
 @Component({
   selector: 'rrcp-career-success',
   styleUrls: ['./career-success.component.scss'],
   templateUrl: './career-success.component.html',
 })
-export class CareerSuccessComponent implements OnDestroy {
+export class CareerSuccessComponent implements OnDestroy, OnInit {
   private alive = true;
 
   solarValue: SolarEnergyStatistics;
 
   devices: Device[];
 
-  constructor(private devicesService: DevicesData, private solarService: SolarData) {
+  constructor(
+    private devicesService: DevicesData,
+    private solarService: SolarData,
+    private sidebarService: NbSidebarService,
+    private layoutService: LayoutService,
+  ) {
     this.devicesService
       .servicesList()
       .pipe(takeWhile(() => this.alive))
@@ -29,6 +35,12 @@ export class CareerSuccessComponent implements OnDestroy {
       .subscribe(data => {
         this.solarValue = data;
       });
+  }
+
+  ngOnInit() {
+    // close menu auto
+    this.sidebarService.toggle(true, 'menu-sidebar');
+    this.layoutService.changeLayoutSize();
   }
 
   changeDeviceStatus(device: Device, isOn: boolean) {
--- src/app/rrcp/home-page/home-page.component.ts
+++ src/app/rrcp/home-page/home-page.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NbSidebarService } from '@nebular/theme';
+import { LayoutService } from '../../@core/utils';
 
 @Component({
   selector: 'rrcp-home-page',
   styleUrls: ['./home-page.component.scss'],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent implements OnDestroy {
+export class HomePageComponent implements OnDestroy, OnInit {
   fullStackInfo: any[];
 
-  constructor() {}
+  constructor(private sidebarService: NbSidebarService, private layoutService: LayoutService) {}
+
+  ngOnInit() {
+    // close menu auto
+    this.sidebarService.toggle(true, 'menu-sidebar');
+    this.layoutService.changeLayoutSize();
+  }
 
   ngOnDestroy() {}
 
